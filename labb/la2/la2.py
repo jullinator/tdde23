fd6bd42c6eb80dc48203266fe622c1752533682e
jullinator <justus.karlsson@hotmail.se>
2017-09-06 15:14:39 +0200

    better lab2

--- labb/la2/la2.py
+++ labb/la2/la2.py
@@ -1,5 +1,6 @@
 #encode = "utf-8"
 import math, numbers
+from functools import reduce
 
 def unit (a):
     """12 -> 2"""
@@ -48,22 +49,13 @@ def check_pnr (pnr_list=[]):
 #WORKED
 def check_pnr_dirty(list):
     """Anropar inga externa funktioner, rörigt"""
-    p_list =[]
-    for i in range(9):
-        mult = (i%2)*-1 +2
-        p_list.append(list[i]*mult)
-    sum = 0
-    for item in p_list:
-        ten = (item - item%10)//10
-        one = item%10
-        sum += ten + one
-    sum_up = 0
-    if sum % 10 == 0:
-        sum_up = sum
-    else:
-        sum_up = (sum - sum%10) + 10
-    return (sum_up - sum) == list[9]
+    mult = lambda x: 2 if x%2 == 0 else 1
+    p_list = [ list[i]*mult(i) for i in range(9)]
+    ten, one = lambda x: (x-x%10)//10, lambda x: x%10               # tiotal-siffran samt entalssiffran
+    sum = reduce(lambda x,y: x + ten(y) + one(y), p_list, 0)        # 0 som initializer:ett måste, annars är sum i början värdet av l[0]
+    sum_up = sum if ( sum%10 == 0 ) else ( sum- sum%10 + 10 )       # runda upp ett tiotal, om den inte redan är på en
+    return (sum_up - sum) == list[9]                                # kalkylerade kontrollsiffran == givna kontrollsiffran?
 
 """TESTER"""
-
+print(check_pnr_dirty([9,6,0,5,2,4,6,8,1,9]))
 #print(sum_ten(5), sum_ten(15))
--- semarie/disk1/datum.py
+++ semarie/disk1/datum.py
@@ -1,3 +1,6 @@
+# encode = utf-8
+from calendar import monthrange
+from datetime import date
 
 A = (30, 1)
 B = (12, 12)
@@ -8,16 +11,18 @@ BDAYS = [A, B, C, D, E]
 
 
 
+
 def closest_bday(mon, day):
     """
     Givet en statisk uppsättning födelsedagar, returnerna namnet på den person som fyller år närmast mon, day.
     Exempel givet A 30/1 39,1 -> A; 1,1-> B; 1,6-> A(om lika, ta nästa)
     """
+    date1= date(2017, mon, day)
     min_mdif = 20
+    #list comprehens
+    #sort, take first.
     for bday in range(len(BDAYS)):
-        mdif = abs(mon- bday[1])
-        if mdif <= min_mdif:
-            min_mdif = mdif
+        pass
 
 
 closest_bday(1,2)
